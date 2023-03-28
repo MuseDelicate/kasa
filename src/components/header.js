332e40e2dce22cd5d1a14ca836f785aed133b247
Morgane Dubouays <dubouays.morgane@gmail.com>
2023-03-28 16:15:31 +0200

    errorPage + homepage ok (desktop)

--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,9 @@ import style from '../CSS/Header.module.css';
 const Header = () => {
     return(
         <header className={style.headerKasa}>
+        <Link to="/">
             <img src={LOGO} alt="logo Kasa" />
+        </Link>
             <nav>
                 <Link to="/" >Accueil</Link>
                 <Link to="/about" >A propos</Link>
